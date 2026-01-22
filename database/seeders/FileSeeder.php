6f2fcdbc299bd1b693b4ec089c46caacc99ab7ab
brightNight1205 <mensreytin1111@gmail.com>
2026-01-22 22:38:33 +0700

    update new

--- database/seeders/FileSeeder.php
+++ database/seeders/FileSeeder.php
@@ -10,10 +10,10 @@ class FileSeeder extends Seeder
     public function run(): void
     {
         // Example 1: Local sample files in storage/app/public/uploads
-        File::create([
-            'file_path' => 'uploads/cutie.jpg',
-            'file_type' => 'jpg'
-        ]);
+        // File::create([
+        //     'file_path' => 'uploads/cutie.jpg',
+        //     'file_type' => 'jpg'
+        // ]);
 
         File::create([
             'file_path' => 'uploads/milo.jpg',
